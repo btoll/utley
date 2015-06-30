21b1613283541722ac564e20f26f3fc060568354
Benjamin Toll <benjam72@yahoo.com>
2015-06-30 17:21:21 -0400

    Initial commit for utley build tool

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "$(tput setaf 2)[INFO]$(tput sgr0) Installing Python build tools..."
+
+ln -s "$PWD"/python3/build/base_compress.py /usr/local/bin/base_compress.py
+ln -s "$PWD"/python3/build/css_compress.py /usr/local/bin/css_compress.py
+ln -s "$PWD"/python3/build/js_compress.py /usr/local/bin/js_compress.py
+ln -s "$PWD"/python3/build/utley.py /usr/local/bin/utley.py
+
+if [ -f ~/.bashrc ]; then
+    echo "export PYTHONPATH=\$PYTHONPATH:/usr/local/bin/" >> ~/.bashrc
+    . ~/.bashrc
+else if [ -f ~/.bash_profile ]; then
+    echo "export PYTHONPATH=\$PYTHONPATH:/usr/local/bin/" >> ~/.bash_profile
+    . ~/.bash_profile
+else
+    export PYTHONPATH=$PYTHONPATH:/usr/local/bin/
+    echo -e "The location of the Python3 build tools has been added to your PYTHONPATH for this terminal session.\nHowever, it's highly recommended to put the following in one of your shell's login scripts:\n\n\texport PYTHONPATH=\$PYTHONPATH:/usr/local/bin/"
+fi
+
+echo
+echo "$(tput setaf 2)[INFO]$(tput sgr0) Installation complete."
+
---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "$(tput setaf 2)[INFO]$(tput sgr0) Removing symbolic links for the build tools..."
+echo
+
+rm /usr/local/bin/base_compress.py
+rm /usr/local/bin/css_compress.py
+rm /usr/local/bin/js_compress.py
+rm /usr/local/bin/utley.py
+
+echo
+echo "$(tput setaf 2)[INFO]$(tput sgr0) Uninstall complete."
+
